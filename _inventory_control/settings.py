2dd12358386528bfa10bf518b03a10b42d5aaf0f
Gabriel Oliveira <gboliveira2503@gmail.com>
2023-01-09 23:16:04 -0300

    fix: settings

--- _inventory_control/settings.py
+++ _inventory_control/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ "0.0.0.0"]
+ALLOWED_HOSTS = ["web-production-8c6f.up.railway.app", "0.0.0.0"]
 
 
 # Application definition
