3b39de7ad68d596bd19930d4619e43442108166f
Gabriel Oliveira <gboliveira2503@gmail.com>
2023-01-10 10:53:30 -0300

    fix: settings

--- _inventory_control/settings.py
+++ _inventory_control/settings.py
@@ -174,4 +174,15 @@ AUTH_USER_MODEL = "accounts.Account"
 
 AUTHENTICATION_BACKENDS = ["accounts.backends.BackendEmail"]
 
-
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Your Project API",
+    "DESCRIPTION": "Your project description",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+}
